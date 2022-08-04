6710901baae7df0ae6f29f28a65a59152f066ebd
Dmitry Lyapin <lyapindm@yandex.ru>
2022-08-04 15:37:59 +0300

    Добавил onClick на кнопках

--- src/components/maze-board/components/field/field.tsx
+++ src/components/maze-board/components/field/field.tsx
@@ -4,21 +4,47 @@ import { getMazeBoardState } from '../../../../services/redux/selectors/maze-boa
 import { fieldBorder, maxWidthBoard } from '../../../../utils/setup-game';
 import style from './field.module.css';
 
+import over from '../../../../images/game-over.svg';
+import ok from '../../../../images/ok.svg';
+import start from '../../../../images/start.svg';
+import { useEffect, useState } from 'react';
+import { Nullable } from '../../../../utils/types/nullable';
 export interface IField {
 	id: string;
 }
 
 export const Field = ({ id }: IField) => {
-	const { axisX, startId } = useAppSelector(getMazeBoardState);
+	const { axisX, startId, endId } = useAppSelector(getMazeBoardState);
+	const [picture, setPicture] = useState<Nullable<string>>(null)
+
+	useEffect(() => {
+		if (startId === id) {
+			setPicture(start)
+		}
+	}, [startId, id])
+
+	const handlerOnClick = () => {
+		if (id === endId) { 
+			setPicture(ok) 
+		} else { 
+			setPicture(over) 
+		}
+	}
 
 	return (
-		<button 
-			className={style.field} 
-			style={{ 
-				width: (maxWidthBoard / axisX) - fieldBorder * 2, 
+		<button
+			className={style.field}
+			onClick={handlerOnClick}
+			style={{
+				width: (maxWidthBoard / axisX) - fieldBorder * 2,
 				height: (maxWidthBoard / axisX) - fieldBorder * 2,
 				margin: fieldBorder
-			}}>{id === startId ? "СТАРТ" : <></>}
+			}}
+		>
+			{
+				picture && <img src={picture} alt='Start, end or fail' />
+			}
+
 		</button >
 	)
 }
