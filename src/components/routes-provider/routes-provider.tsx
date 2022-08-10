4397262244b12736628d80e58cd2b3402640fd74
Dmitry Lyapin <lyapindm@yandex.ru>
2022-08-10 15:30:25 +0300

    Отключил ProtectedRoute

--- src/components/routes-provider/routes-provider.tsx
+++ src/components/routes-provider/routes-provider.tsx
@@ -1,12 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { GameMaze } from '../../pages/game-maze-page/game-maze-page';
 import { NotFoundPage } from '../../pages/not-found-page/not-found';
-import { ProtectedRoute } from './components/protected-route';
 
 export const RoutesProvider = () => {
   return (
     <Routes>
-      <Route path='/' element={<ProtectedRoute redirect='/login' element={<GameMaze />} />} />
+      <Route path='/' element={<GameMaze />} />
       <Route path='*' element={<NotFoundPage />} />
     </Routes>)
 };
