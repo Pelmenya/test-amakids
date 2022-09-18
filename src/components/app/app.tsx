b62cf9e583e9f9cbaf43d1654acfba335354bf69
Dmitry Lyapin <lyapindm@yandex.ru>
2022-09-18 12:43:33 +0300

    Убрал роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,11 @@
-import { RoutesProvider } from '../routes-provider/routes-provider';
+import { GameMaze } from '../../pages/game-maze-page/game-maze-page';
 
 import style from './app.module.css';
 
 export const App = () => {
   return (
     <div className={style.app}>
-      <RoutesProvider />
+      <GameMaze />
     </div>
   );
 };
